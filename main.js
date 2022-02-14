88e878ce59ccb9cbe0d40cf4bc202ce338b64749
Pablo <64982788+pabl0sh@users.noreply.github.com>
2022-02-14 09:46:48 +0200

    START
    beta v0.1

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let PN = document.getElementById('PN');
+let FT = document.getElementById('FT');
+
+function find_procent (x, y) {
+    return x.value / y.value * 100
+}
+
+
+
+
+
+
